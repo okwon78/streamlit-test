c3697671bb1126f49b7f4b5fe813173831e0d9c4
Obiwan <ob.kwon@dreamus.io>
2025-03-23 18:30:28 +0900

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
+@st.cache_data
 def load_data(csv_file):
     data = pd.read_csv(csv_file)
     return data
@@ -15,7 +15,11 @@ def main():
     data = load_data(csv_file=csv_file)
     data_load_state.text("Loading data...done!")
     # data_load_state.text("Done! (using st.cache_data)")
+    st.subheader('Raw data')
+    st.write(data)
 
+    hist_values = np.histogram(
+    data["1학년 학생수"].dt.hour, bins=24, range=(0,24))[0]
 
 if __name__ == "__main__":
     main()
